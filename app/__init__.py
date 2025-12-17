b0aadb5e3c91b1a92691aa3cb468ce2798840d90
RohanSen08 <rohanhenrysen@gmail.com>
2025-12-17 11:05:11 -0500

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -147,13 +147,19 @@ c.execute("INSERT OR IGNORE INTO dialogue(encounter_type, response1, response2, 
 c.execute("INSERT OR IGNORE INTO dialogue(encounter_type, response1, response2, response3, response4) VALUES (?, ?, ?, ?, ?)", ("trivia", "Wow, the cat is impressed!", "The cat looks down on your knowledge bank.", "Amazing!", "Nope."))
 
 def pick_city():
-    with open("app/locations", "r") as f:
-        lines = f.read().strip().splitlines()
-        city,lat,lon = random.choice(lines).split(",")
-    with open("app/keys/key_pirateWeather.txt", "r") as f:
-        key = f.read().strip()
-    with urllib.request.urlopen(f"https://api.pirateweather.net/forecast/{key}/{lat},{lon}") as response:
-        a = json.loads(response.read())
+    while True:
+        try:
+            with open("app/locations", "r") as f:
+                lines = f.read().strip().splitlines()
+                city,lat,lon = random.choice(lines).split(",")
+                print(city)
+            with open("app/keys/key_pirateWeather.txt", "r") as f:
+                key = f.read().strip()
+            with urllib.request.urlopen(f"https://api.pirateweather.net/forecast/{key}/{lat},{lon}") as response:
+                a = json.loads(response.read())
+            break
+        except:
+            print("error fetching weather")
     return a, city
 
 def get_time(a):
@@ -303,7 +309,7 @@ def profile():
         c = db.cursor()
         c.execute("SELECT * FROM user_profile WHERE username = ?", (session["username"],))
         user = c.fetchone()
-        
+
         if user is None:
             session.pop("username")
             return redirect(url_for('login'))
@@ -313,7 +319,7 @@ def profile():
             c.execute("UPDATE user_profile SET sprite = ? WHERE username = ?", (icon, session["username"]))
             db.commit()
             return redirect(url_for('profile'))
-        
+
         c.execute("SELECT cat, affection, level FROM user_encounters WHERE username = ?", (session["username"],))
         infoRow = c.fetchall()
 
@@ -385,53 +391,53 @@ def weatherencounters(breed):
             encounter = map_info(a)
             # get energy level for weather
             currNRG = encounter[2]
-            
+
             # get cats for energy level
             c.execute("SELECT * FROM cats WHERE energy_lvl = ?", (currNRG,))
             cats = c.fetchall()
-            
+
             # get a random cat
             myCatRow = random.choice(cats)
-            
+
             # get values
             breed = myCatRow[0]
             jokeDiff = myCatRow[2] #difficulty in int for jokes
             trivDiff = myCatRow[3] #difficulty in text for trivia
-            
+
             # set default vals for joke and triv
             currJoke = None
             currTriv = None
-            
+
             # 0 for joke
             # 1 for trivia
             if myCatRow[4] == 0:
                 # now pull from jokes tbl for response 1 and 2
                 # check difficulty or difficulty2 of cat for joke
                 c.execute("SELECT joke FROM jokes WHERE difficulty <= ? ORDER BY RANDOM() LIMIT 1", (jokeDiff,))
-                
+
                 # category, joke, difficulty, desired_response
                 currJoke = c.fetchone()
-                
+
                 # get dialogue
                 c.execute("SELECT response1, response2, response3, response4 FROM dialogue WHERE encounter_type = ?", ("joke",))
                 dialogue = c.fetchone()
-                
+
             else: #elif myCatRow[4] == 1:
                 # for trivia options, pull from trivia tbl
                 # check difficulty or difficulty2 of cat for trivia
                 c.execute("SELECT question FROM trivia WHERE difficulty <= ? ORDER BY RANDOM() LIMIT 1", (trivDiff,))
-                
+
                 # difficulty, question, a1, a2, a3, a4, correct
                 currTriv = c.fetchone()
-                
+
                 # get dialogue
                 c.execute("SELECT response1, response2, response3, response4 FROM dialogue WHERE encounter_type = ?", ("trivia",))
                 dialogue = c.fetchone()
-            
+
             # ------CHECKING AFFECTION-----
             # using breed for now, but may need ID for each cat***(NEEDS FIX)
             cat = myCatRow[0]
-            
+
             # if pressed answer == currTrivia[6], add affection
             # leave encounter after clicking answer
             c.execute("SELECT affection, level FROM user_encounters WHERE username = ? AND cat = ?", (session["username"], breed))
@@ -444,22 +450,22 @@ def weatherencounters(breed):
             else:
                 newAffec = thisEncounter[0]
                 lvl = thisEncounter[1]
-            
+
             # if get the answer right in trivia (use buttons for prompts)
             # add 10 extra points from interaction
             # newAffec += 10
             # ------INCOMPLETE-------
-            
+
             # if interact, get affectoin thru energy_lvl
             addedAffec = myCatRow[1] * random.randint(1,6)
             newAffec += addedAffec
-            
+
             # keep track of whether or not level updates
             lev = False
             if newAffec >= 100:
                 lvl += 1
                 newAffec -= 100
-            
+
             c.execute("INSERT OR REPLACE INTO user_encounters(username, cat, affection, level) VALUES (?, ?, ?, ?)", (session["username"], breed, newAffec, lvl))
     return weatherspage(weather, currJoke, currTriv, myCatRow, dialogue, breed, addedAffec, lvl)
 
